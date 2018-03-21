d366f14489c896fbb87244b12a10b02de8d58b9a
Unknown <raul.de.heer.galisteo@fronter.summacollege.nl>
2018-03-21 12:50:13 +0100

    Add entities

---
+++ backend/src/Entity/AcademyReview.php
@@ -0,0 +1,51 @@
+<?php
+namespace Ludum\Entity;
+
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use JsonSerializable;
+
+/**
+ * @ORM\Table(name="academy_reviews")
+ * @ORM\Entity
+ */
+class AcademyReview implements JsonSerializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="integer", length=1)
+     */
+    public $stars;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    public $review;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    public $creation_date;
+
+    public function __construct()
+    {
+        $this->creation_date = new DateTime('now');
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'stars' => $this->stars,
+            'review' => $this->review,
+            'creation_date' => $this->creation_date->format('Y-m-d H:i')
+        ];
+    }
+}
---
+++ backend/src/Entity/Promotions.php
@@ -0,0 +1,56 @@
+<?php
+namespace Ludum\Entity;
+
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use JsonSerializable;
+
+/**
+ * @ORM\Table(name="promotions")
+ * @ORM\Entity
+ */
+class Promotions implements JsonSerializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    public $start_date;
+
+    /**
+     * @ORM\Column(type="integer", length=3)
+     */
+    public $run_time;
+
+    /**
+     * @ORM\Column(type="integer", length=3)
+     */
+    public $radius;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    public $budget;
+
+    public function __construct()
+    {
+        $this->start_date = new DateTime();
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->start_date->format('Y-m-d H:i'),
+            'run_time' => $this->run_time,
+            'radius' => $this->radius,
+            'budget' => $this->budget
+        ];
+    }
+}
